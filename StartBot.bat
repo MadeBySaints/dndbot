c451ae50e3c164ed179ba25f0636ec204056cf9e
MadeBySaints <MadeBySaints@users.noreply.github.com>
2018-07-26 00:05:14 -0400

    Update StartBot.bat

--- StartBot.bat
+++ StartBot.bat
@@ -1,5 +1,5 @@
 @echo off
-title Dungeons and Discords Bot - By MadeBySaints
+title Dungeons and Discords - By MadeBySaints
 color 2
 cd "C:\Users\MadeBySaints\Desktop\Tools\dndbot"
 node --harmony main.js
